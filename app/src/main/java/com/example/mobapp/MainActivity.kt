f96e8cb09608846d34ef31cb7e9a768bc3b862fc
Tomáš Zahradník <>
2024-12-03 20:55:14 +0100

    Permise, aktualizace jvm a knihoven, zakladni struktura Mainu

--- app/src/main/java/com/example/mobapp/MainActivity.kt
+++ app/src/main/java/com/example/mobapp/MainActivity.kt
@@ -1,47 +1,79 @@
 package com.example.mobapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mobapp.ui.theme.MobAppTheme
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.mobapp.databinding.MainActivityBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var viewBinding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MobAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        viewBinding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (allPermissionsGranted()) {
+            // zapni kameru a fachej
+        } else {
+            requestPermissions()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobAppTheme {
-        Greeting("Android")
+    override fun onPause() {
+        super.onPause()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE
+            ).toTypedArray()
+    }
+
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        )
+        { permissions ->
+            var permissionGranted = true
+            var cameraGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    if (it.key == Manifest.permission.CAMERA) {
+                        cameraGranted = false
+                    } else {
+                        permissionGranted = false
+                    }
+            }
+            if (permissionGranted && cameraGranted) {
+                // prace s kamerou
+            } else if (permissionGranted) {
+                // prace bez kamery - z obrazku?
+            } else {
+                // prace bez ostatnich prvku - je template uz stazen?
+            }
+        }
 }
